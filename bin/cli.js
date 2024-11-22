12b95ca65cba7b5ad7c33a6cef9926bb6a7d2713
akash100a <arakash968786@gmail.com>
2024-11-22 22:06:00 +0600

    up

--- bin/cli.js
+++ bin/cli.js
@@ -11,15 +11,23 @@ const runCommand = command =>{
 }
 const repoName = process.argv[2];
 console.log(repoName);
-const gitCheckoutCommand = `git clone --dept 1 https://github.com/arakash-developer/reactjs-start ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const repoName2 = process.argv[1].split('\\');
+repoName2.pop('cli.js');
+// console.log(my);
+// repoName2.push('a')
+let newa = repoName2.join("/")
+// console.log(repoName2);
+console.log(newa);
+
+// let mypath = `C:/Users/my-pc/Documents/GitHub/react-starter/a`;
+const gitCheckoutCommand = `git clone https://github.com/arakash-developer/reactjs-start.git ${newa}`;
+// const installDepsCommand = `cd ${repoName} && npm install`;
 // console.log(`Cloning the repository with name ${repoName}`);
 console.log(`Installing Packages for ${repoName} @akash.dev`);
 const checkedOut =  runCommand(gitCheckoutCommand);
 if(!checkedOut) process.exit(-1);
 console.log(`Installing dependencies for ${repoName} @akash.dev`);
-const installedDeps = runCommand(installDepsCommand);
-if(!installedDeps) process.exit(-1); 
+// const installedDeps = runCommand(installDepsCommand);
+// if(!installedDeps) process.exit(-1); 
 console.log(`Congratulation!! You are Ready @akash.dev`);
 console.log(`cd ${repoName} && npm run dev or npm start @akash.dev`);
-
